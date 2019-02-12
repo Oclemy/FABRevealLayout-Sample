2bfd22f781aa3a9cd2b70aa40f4f20c5d2f7ab64
Oclemy <oclemmi@gmail.com>
2019-02-12 04:42:50 +0300

    Initial commit

---
+++ app/src/main/java/info/camposha/mfabreveallayout/MainActivity.java
@@ -0,0 +1,42 @@
+package info.camposha.mfabreveallayout;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.truizlop.fabreveallayout.FABRevealLayout;
+import com.truizlop.fabreveallayout.OnRevealChangeListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FABRevealLayout fabRevealLayout = findViewById(R.id.fab_reveal_layout);
+        configureFABReveal(fabRevealLayout);
+    }
+
+    private void configureFABReveal(FABRevealLayout fabRevealLayout) {
+        fabRevealLayout.setOnRevealChangeListener(new OnRevealChangeListener() {
+            @Override
+            public void onMainViewAppeared(FABRevealLayout fabRevealLayout, View mainView) {}
+
+            @Override
+            public void onSecondaryViewAppeared(final FABRevealLayout fabRevealLayout, View secondaryView) {
+                prepareBackTransition(fabRevealLayout);
+            }
+        });
+    }
+
+    private void prepareBackTransition(final FABRevealLayout fabRevealLayout) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                fabRevealLayout.revealMainView();
+            }
+        }, 2000);
+    }
+
+}
